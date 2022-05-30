61c87d07979725ba6c0cdbddf90316c17be0900e
Iammehran72 <106099427+Iammehran72@users.noreply.github.com>
2022-05-30 18:40:14 +0530

    Create additionofintegers.c

---
+++ additionofintegers.c
@@ -0,0 +1,14 @@
+//Addition of two integers//
+#include<stdio.h>
+int main()
+{
+	
+	int a, b, sum;
+	printf("Please Enter Two Integers");
+	scanf("%d %d ", &a, &b);
+	sum=a+b;
+	
+	printf("The Addition Of %d and %d is %d ", a, b, sum);
+	
+	return 0;
+}
