2eed8157faf79760e0f7da0c852e9f5e0b3117ba
Iammehran72 <106099427+Iammehran72@users.noreply.github.com>
2022-06-02 20:10:46 +0530

    Update additionofintegers.c

--- additionofintegers.c
+++ additionofintegers.c
@@ -4,11 +4,13 @@ int main()
 {
 	
 	int a, b, sum;
-	printf("Please Enter Two Integers");
+	printf("Please Enter Two Integers : ");
 	scanf("%d %d ", &a, &b);
 	sum=a+b;
 	
 	printf("The Addition Of %d and %d is %d ", a, b, sum);
+	
+	getchar();
 	
 	return 0;
 }
